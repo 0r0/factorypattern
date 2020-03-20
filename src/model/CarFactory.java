008b4ff1530b65979a244da727001fb70d0daea1
0r0 <0r0.developer@gmail.com>
2020-03-21 02:15:38 +0430

    inital car factory class

---
+++ src/model/CarFactory.java
@@ -0,0 +1,26 @@
+package model;
+
+public class CarFactory  {
+    public static Car carBuilder(CarType model) throws Exception {
+        Car car=null;
+        switch (model){
+            case SEDAN:
+                car=new SedanCar();
+                break;
+            case SMALL:
+                car=new SmallCar();
+                break;
+            case LUXURY:
+                car=new LuxuryCar();
+                break;
+            default:
+            {
+                System.out.println("this type car is not defined");
+                throw new Exception("fdf");
+            }
+
+
+        }
+        return car;
+    }
+}
