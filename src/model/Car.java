8178fda3d7653beb7bc421bfffd5acd19bf7507f
0r0 <0r0.developer@gmail.com>
2020-03-21 02:15:10 +0430

    inital car abstract class

---
+++ src/model/Car.java
@@ -0,0 +1,17 @@
+package model;
+
+public abstract class Car {
+    private CarType model;
+    public Car(CarType model){
+        this.model=model;
+    }
+    protected  abstract  void   construct();
+
+    public CarType getModel() {
+        return model;
+    }
+
+    public void setModel(CarType model) {
+        this.model = model;
+    }
+}
